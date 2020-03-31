ec099cba32161a64022c8a32d1ea9a7556d2a51d
Ryan Birmingham <birm@rbirm.us>
2020-03-31 01:32:45 -0400

    clean up style of tests

--- test/unit/authHandlers.js
+++ test/unit/authHandlers.js
@@ -6,31 +6,31 @@ process.env.DISABLE_SEC = 'true';
 
 describe('filterHandler', function() {
   it('Filters on a given field', function(done) {
-    var req = {}
-    var res = {}
-    req.filter = ["LOW_SEC"]
+    var req = {};
+    var res = {};
+    req.filter = ['LOW_SEC'];
     req.data = [
       {'name': 'public'},
-      {'name': 'low security', 'filter': "LOW_SEC"},
-      {'name': 'high security', 'filter': "HIGH_SEC"}
-    ]
+      {'name': 'low security', 'filter': 'LOW_SEC'},
+      {'name': 'high security', 'filter': 'HIGH_SEC'},
+    ];
     this.timeout(5000);
-    auth.filterHandler('data', 'filter', 'filter')(req, res, function(){});
+    auth.filterHandler('data', 'filter', 'filter')(req, res, function() {});
     (req.data).should.be.an('array');
     (req.data.length).should.be.eql(2);
     done();
   });
   it('Returns all for special command **', function(done) {
-    var req = {}
-    var res = {}
-    req.filter = ["**"]
+    var req = {};
+    var res = {};
+    req.filter = ['**'];
     req.data = [
       {'name': 'public'},
-      {'name': 'low security', 'filter': "LOW_SEC"},
-      {'name': 'high security', 'filter': "HIGH_SEC"}
-    ]
+      {'name': 'low security', 'filter': 'LOW_SEC'},
+      {'name': 'high security', 'filter': 'HIGH_SEC'},
+    ];
     this.timeout(5000);
-    auth.filterHandler('data', 'filter', 'filter')(req, res, function(){});
+    auth.filterHandler('data', 'filter', 'filter')(req, res, function() {});
     (req.data).should.be.an('array');
     (req.data.length).should.be.eql(3);
     done();
--- test/unit/permissionHandler.js
+++ test/unit/permissionHandler.js
@@ -4,20 +4,20 @@ var should = chai.should();
 
 describe('permissionHandler', function() {
   it('Accepts if ok', function(done) {
-    var req = {}
-    var res = {}
-    req.tokenInfo = {"userType" :"nice"}
+    var req = {};
+    var res = {};
+    req.tokenInfo = {'userType': 'nice'};
     this.timeout(5000);
-    permissionHandler(["nice"], true)(req, res, function(){});
+    permissionHandler(['nice'], true)(req, res, function() {});
     (req.permission_ok).should.be.eql(true);
     done();
   });
   it('Rejects if not', function(done) {
-    var req = {}
-    var res = {}
-    req.tokenInfo = {"userType" :"naughty"}
+    var req = {};
+    var res = {};
+    req.tokenInfo = {'userType': 'naughty'};
     this.timeout(5000);
-    permissionHandler(["nice"], true)(req, res, function(){});
+    permissionHandler(['nice'], true)(req, res, function() {});
     (req.permission_ok).should.be.eql(false);
     done();
   });
