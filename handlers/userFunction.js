2e939853f16b76d3ed40b6873094a16171f4e6c3
Ryan Birmingham <birm@rbirm.us>
2020-04-01 18:38:55 -0400

    option for no public access

--- handlers/userFunction.js
+++ handlers/userFunction.js
@@ -1,16 +1,22 @@
+var ALLOW_PUBLIC = process.env.ALLOW_PUBLIC;
+
 var dataHandlers = require('./dataHandlers.js');
 // userFunction -- used for login given id provider token
 function userFunction(token) {
   return new Promise(function(res, rej) {
     dataHandlers.User.forLogin(token.email).then((x)=>{
       if (x.length <= 0) {
-        const publicToken = {};
-        publicToken.userFilter = ['Public'];
-        publicToken.userType = 'Null';
-        publicToken.email = token.email;
-        publicToken.name = token.name;
-        publicToken.picture = token.picture;
-        res(publicToken);
+        if (ALLOW_PUBLIC){
+          const publicToken = {};
+          publicToken.userFilter = ['Public'];
+          publicToken.userType = 'Null';
+          publicToken.email = token.email;
+          publicToken.name = token.name;
+          publicToken.picture = token.picture;
+          res(publicToken);
+        } else {
+          rej(new Error("Public users not allowed on this instance"));
+        }
       } else {
         const newToken = {};
         newToken.userType = x[0].userType || 'Null';
