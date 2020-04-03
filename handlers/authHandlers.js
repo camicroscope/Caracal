342c29b5827cb4f162b4ce06d6bb1a2027cfa2d1
Ryan Birmingham <birm@rbirm.us>
2020-04-02 20:46:56 -0400

    bypass all in zero case too

--- handlers/authHandlers.js
+++ handlers/authHandlers.js
@@ -211,16 +211,19 @@ function editHandler(dataField, filterField, attrField) {
     // edit routes should operate on one object
     if (req[dataField].length == 1) {
       // DESTROY query
-      for (let n in req.query){
-        if (req.query.hasOwnProperty(n)){
-          delete req.query[n]
+      for (let n in req.query) {
+        if (req.query.hasOwnProperty(n)) {
+          delete req.query[n];
         }
       }
       req.query = {_id: req[dataField][0]._id['$oid']};
     }
     // don't error if 1 or 0 objects
     if (req[dataField].length <= 1) {
-      next();
+      let errorMessage = {};
+      errorMessage.error = 'Nothing applicable to change.';
+      errorMessage.statusCode = 200;
+      next(errorMessage);
     } else {
       let errorMessage = {};
       errorMessage.error = 'At most one document may be changed at once.';
