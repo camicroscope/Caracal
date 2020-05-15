6e28690eca255b7b1446bc8679d0fdd2e37dd57e
Ryan Birmingham <birm@rbirm.us>
2020-05-15 17:44:31 -0400

    show reason for rejection

--- handlers/sanitizeHandler.js
+++ handlers/sanitizeHandler.js
@@ -10,7 +10,8 @@ function sanitizeBody(req, res, next){
   // handle req body edgecases
   if (ERR_ON_SANITIZE){
     if (req.body.indexOf("<") >=0 || req.body.indexOf(">") >=0){
-      let e = new Error("Characters < and > are disallowed.");
+      let e = {'statusCode': 400};
+      e.error = 'Characters < and > disallowed in body.';
       next(e)
     } else {
       next()
