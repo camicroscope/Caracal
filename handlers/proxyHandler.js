82a353b5f2e61669b83448c99ff483c69d6dd249
Ryan Birmingham <birm@rbirm.us>
2020-06-12 17:50:24 -0400

    change n?

--- handlers/proxyHandler.js
+++ handlers/proxyHandler.js
@@ -1,6 +1,7 @@
 var proxy = require('http-proxy-middleware');
 
-proxyHandler = function(PATH){
+proxyHandler = function(target, n){
+  n = n || 2
   return function(req, res, next) {
     proxy({
       secure: false,
@@ -10,13 +11,13 @@ proxyHandler = function(PATH){
         next(err);
       },
       changeOrigin: true,
-      target: PATH,
+      target: target,
       pathRewrite: function(path, req) {
-        console.log(PATH)
+        console.log(target)
         console.log(path)
         // NOTE -- this may need to change if the original url has more subdirs or so added
         var splitPath = path.split('/');
-        return '/' + splitPath.slice(2, splitPath.length).join('/');
+        return '/' + splitPath.slice(n, splitPath.length).join('/');
       },
       onProxyReq: function(proxyReq, req, res) {
         if (req.method == 'POST') {
