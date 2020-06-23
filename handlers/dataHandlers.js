46fd6ad0a0e1dcd06f066c00f2aa2befb52d9ee9
Ryan Birmingham <birm@rbirm.us>
2020-06-23 15:57:41 -0400

    style fix

--- handlers/dataHandlers.js
+++ handlers/dataHandlers.js
@@ -248,7 +248,6 @@ Mark.spatial = function(req, res, next) {
 
 Mark.multi = function(req, res, next) {
   var query = req.query;
-  console.log('before: ', JSON.stringify(query))
   if (query.nameList) {
     query['provenance.analysis.execution_id'] = {
       '$in': JSON.parse(query.nameList),
@@ -278,7 +277,6 @@ Mark.multi = function(req, res, next) {
       '$lt': parseFloat(query.footprint),
     };
   }
-  console.log('after: ', JSON.stringify(query))
   mongoFind('camic', 'mark', query).then((x) => {
     req.data = x;
     next();
