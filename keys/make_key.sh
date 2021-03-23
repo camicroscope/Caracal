a5e0804a21eeb5008ffa4a34d3a45fdf9768bdfe
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-03-23 14:51:48 +0000

    fix(keys): always generate keys in /keys, fixes #84
    - add shebang to specify shell
    - not kept as /usr/bin/bash because some systems can have bash at a different location
    - add directory to each file being generated

--- keys/make_key.sh
+++ keys/make_key.sh
@@ -1,1 +1,6 @@
-openssl req -subj '/CN=www.camicroscope.com/O=caMicroscope Local Instance Key./C=US' -x509 -nodes -newkey rsa:2048 -keyout key -out key.pub
+#!/usr/bin/env bash
+
+# to generate keys only in the /keys directory.
+PWD="$(dirname "$0")"
+
+openssl req -subj '/CN=www.camicroscope.com/O=caMicroscope Local Instance Key./C=US' -x509 -nodes -newkey rsa:2048 -keyout $PWD/key -out $PWD/key.pub
