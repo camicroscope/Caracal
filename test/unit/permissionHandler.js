495a14be41e2d008dfe990e514489d5ca32558d6
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-07-07 00:55:58 +0000

    chore(permissionHandler): update tests for handler

--- test/unit/permissionHandler.js
+++ test/unit/permissionHandler.js
@@ -1,25 +1,36 @@
-const chai = require('chai');
-const permissionHandler = require('../../handlers/permssionHandler.js');
+/* eslint-disable no-unused-vars */
 
+const chai = require('chai');
+const { describe, it } = require('mocha');
 const should = chai.should();
 
+/**
+ * loading services
+ *
+ * roles => to summon all defined roles in the system
+ * permissionHandler => to defined permission rules on resources
+ */
+const { ROLE } = require('../../service/roles/roles');
+const permissionHandler = require('../../handlers/permssionHandler');
+
 describe('permissionHandler', () => {
-  it('Accepts if ok', function (done) {
+  it('should accept if OK', (done) => {
     const req = {};
     const res = {};
-    req.tokenInfo = { userType: 'nice' };
+    req.tokenInfo = { userType: ROLE.ADMIN };
     this.timeout(5000);
-    permissionHandler(['nice'], true)(req, res, () => {});
-    (req.permission_ok).should.be.eql(true);
+    permissionHandler([ROLE.ADMIN], true)(req, res, () => {});
+    req.permission_ok.should.be.eql(true);
     done();
   });
-  it('Rejects if not', function (done) {
+
+  it('should reject if not OK', (done) => {
     const req = {};
     const res = {};
-    req.tokenInfo = { userType: 'naughty' };
+    req.tokenInfo = { userType: ROLE.VISITOR };
     this.timeout(5000);
-    permissionHandler(['nice'], true)(req, res, () => {});
-    (req.permission_ok).should.be.eql(false);
+    permissionHandler([ROLE.ADMIN], true)(req, res, () => {});
+    req.permission_ok.should.be.eql(false);
     done();
   });
 });
