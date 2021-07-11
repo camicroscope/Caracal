61d34ee32974c288972561148f712d85bac33d3a
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-07-12 04:46:01 +0530

    feat(roles): add definitions

--- service/roles/definitions.js
+++ service/roles/definitions.js
@@ -5,23 +5,75 @@ const { RESOURCE } = require('./resources');
 const controller = new AccessControl();
 
 /** any un-authenticated user */
-controller.grant(ROLE.VISITOR).readAny('slide');
+controller
+  .grant(ROLE.VISITOR)
+  .read(RESOURCE.MIDDLEWARE('loader.proxyHandler'))
+  .read(RESOURCE.MIDDLEWARE('googleAuth'))
+  .read(RESOURCE.MIDDLEWARE('img.iip.raw'))
+  .read(RESOURCE.MIDDLEWARE('data'))
+  .read(RESOURCE.SLIDE())
+  .create(RESOURCE.REQUEST())
+  .read(RESOURCE.MARK())
+  .read(RESOURCE.MARK('types'))
+  .read(RESOURCE.MARK('multi'))
+  .read(RESOURCE.MARK('spatial'))
+  .read(RESOURCE.MARK('findMarkTypes'))
+  .create(RESOURCE.PRESET_LABEL())
+  .update(RESOURCE.PRESET_LABEL())
+  .delete(RESOURCE.PRESET_LABEL())
+  .read(RESOURCE.TEMPLATE())
+  .read(RESOURCE.HEATMAP())
+  .read(RESOURCE.HEATMAP('types'))
+  .read(RESOURCE.HEATMAP_EDIT())
+  .read(RESOURCE.LOG())
+  .create(RESOURCE.LOG())
+  .read(RESOURCE.FREEFORM())
+  .create(RESOURCE.FREEFORM())
+  .read(RESOURCE.CONFIGURATION())
+  .read(RESOURCE.USER())
+  .read(RESOURCE.USER('wcido'));
 
 /** roles of user in system */
 controller
   .grant(ROLE.EDITOR)
   .extend(ROLE.VISITOR)
-  .createOwn(RESOURCE.SLIDE)
-  .updateOwn(RESOURCE.SLIDE)
-  .deleteOwn(RESOURCE.SLIDE);
+  .read(RESOURCE.MIDDLEWARE('loader.login'))
+  .read(RESOURCE.MIDDLEWARE('loader.slide.delete'))
+  .read(RESOURCE.MIDDLEWARE('list'))
+  .create(RESOURCE.SLIDE())
+  .delete(RESOURCE.SLIDE())
+  .update(RESOURCE.SLIDE())
+  .read(RESOURCE.REQUEST())
+  .delete(RESOURCE.REQUEST())
+  .create(RESOURCE.MARK())
+  .update(RESOURCE.MARK())
+  .delete(RESOURCE.MARK())
+  .create(RESOURCE.TEMPLATE())
+  .update(RESOURCE.TEMPLATE())
+  .delete(RESOURCE.TEMPLATE())
+  .create(RESOURCE.HEATMAP())
+  .update(RESOURCE.HEATMAP())
+  .delete(RESOURCE.HEATMAP())
+  .create(RESOURCE.HEATMAP_EDIT())
+  .update(RESOURCE.HEATMAP_EDIT())
+  .delete(RESOURCE.HEATMAP_EDIT())
+  .delete(RESOURCE.LOG())
+  .update(RESOURCE.LOG())
+  .update(RESOURCE.FREEFORM())
+  .delete(RESOURCE.FREEFORM())
+  .create(RESOURCE.CONFIGURATION())
+  .delete(RESOURCE.CONFIGURATION())
+  .update(RESOURCE.CONFIGURATION())
+  .create(RESOURCE.USER())
+  .delete(RESOURCE.USER())
+  .update(RESOURCE.USER())
+  .read(RESOURCE.WORKBENCH('uploadDataset'))
+  .read(RESOURCE.WORKBENCH('trainModel'))
+  .read(RESOURCE.WORKBENCH('deleteUserData'))
+  .read(RESOURCE.WORKBENCH('modelDownload'));
 
 /** roles of moderator in system */
-controller
-  .grant(ROLE.ADMIN)
-  .extend(ROLE.EDITOR)
-  .createAny(RESOURCE.SLIDE)
-  .updateAny(RESOURCE.SLIDE)
-  .deleteAny(RESOURCE.SLIDE);
+controller.grant(ROLE.ADMIN).extend(ROLE.EDITOR);
 
 /**
  * Displays the current configuration of roles and list all the resources on the console on startup
--- service/roles/resources.js
+++ service/roles/resources.js
@@ -1,21 +1,33 @@
 /**
+ * Helper function to allow using sub sections in the resource types.
+ * which are strictly not defined as resource types.
+ *
+ * This allows quickly adding new routes / resources to operate in the application.
+ *
+ * @param {string} head => the name of the resource
+ * @param {string} tail => the sub section of the resource apart from basic CRUD
+ */
+const fx = (head, tail) => `${head}${tail ? `.${tail}` : ''}`;
+
+/**
  * RESOURCE represents various entities that are operated in the application.
  * This object is used to define role definitions with a consistent style
  */
 const RESOURCE = {
-  LOG: 'log',
-  USER: 'user',
-  MARK: 'mark',
-  SLIDE: 'slide',
-  REQUEST: 'request',
-  FREEFORM: 'freeform',
-  TEMPLATE: 'template',
-  WORKBENCH: 'workbench',
-  COLLECTION: 'collection',
-  MIDDLEWARE: 'middleware',
-  PRESET_LABEL: 'preset_label',
-  HEATMAP_EDIT: 'heatmap_edit',
-  CONFIGURATION: 'configuration',
+  LOG: (x) => fx('log', x),
+  USER: (x) => fx('user', x),
+  MARK: (x) => fx('mark', x),
+  SLIDE: (x) => fx('slide', x),
+  HEATMAP: (x) => fx('heatmap', x),
+  REQUEST: (x) => fx('request', x),
+  FREEFORM: (x) => fx('freeform', x),
+  TEMPLATE: (x) => fx('template', x),
+  WORKBENCH: (x) => fx('workbench', x),
+  COLLECTION: (x) => fx('collection', x),
+  MIDDLEWARE: (x) => fx('middleware', x),
+  PRESET_LABEL: (x) => fx('presetLabel', x),
+  HEATMAP_EDIT: (x) => fx('heatmapEdit', x),
+  CONFIGURATION: (x) => fx('configuration', x),
 };
 
 module.exports = { RESOURCE };
