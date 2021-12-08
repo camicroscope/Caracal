ef260e0a6cf85777ae618586d4f290161be1d888
Nan Li <nan.li@emory.edu>
2021-12-07 20:06:38 -0500

    remove test code ~~~

--- caracal.js
+++ caracal.js
@@ -260,13 +260,13 @@ var mailOptions = {
   html: "<b>Hello world?</b>",
 };
 
- const test = await transporter.sendMail(mailOptions, function(error, info) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
+//  const test = await transporter.sendMail(mailOptions, function(error, info) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
 
 
 //
