ae1081920f089c6c5367a0a718fceeff61f80886
Ryan Birmingham <birm@rbirm.us>
2022-02-24 17:26:09 -0500

    bring in signing key

--- handlers/pathdbIipHandler.js
+++ handlers/pathdbIipHandler.js
@@ -4,54 +4,54 @@ const axios = require('axios');
 var jwt = require('jsonwebtoken');
 var EXPIRY = process.env.EXPIRY || '1d';
 var BYPASS_IIP_CHECK = process.env.BYPASS_IIP_CHECK == "Y";
+const auth = require('./authHandlers.js')
+
 
 // internal function to issue a new jwt
-function issueToken(data, signKey) {
+function issueToken(data, signKey){
   return jwt.sign(data, signKey, {
     algorithm: 'RS256',
     expiresIn: EXPIRY,
   });
 }
 
-slideTokenGen = function(signKey) {
-  return function(req, res, next) {
-    if (req.query.slide) {
-      // url for checking if user has access to this slide
-      const PDB_URL = process.env.PDB_URL ||`http://quip-pathdb/`;
-      let lookupUrl = PDB_URL + "/node/" + req.query.slide + "?_format=json";
-      axios.get(lookupUrl, {headers: req.headers}).then((x)=>{
-        // get path
-        if (x && x['field_iip_path'] && x['field_iip_path'].length && x['field_iip_path']['value']) {
-          let filepath = x['field_iip_path']['value'];
-          // issue token including this slidepath as activeSlide
-          let token = req.tokenInfo;
-          token.activeSlide = filepath;
-          res.data = issueToken(token, signKey);
-          next();
-        } else {
-          // do not issue token
-          let err = {};
-          err.message = "unauthorized token request";
-          err.statusCode = 401;
-          next(err);
-        }
-      }).catch((e)=>{
-        console.error(e);
-        next(e);
-      });
-    } else {
-      let err = {};
-      err.message = "malformed token request";
-      err.statusCode = 400;
-      next(err);
-    }
-  };
+slideTokenGen = function(req, res, next){
+  if (req.query.slide){
+    // url for checking if user has access to this slide
+    const PDB_URL = process.env.PDB_URL ||`http://quip-pathdb/`;
+    let lookupUrl = PDB_URL + "/node/" + req.query.slide + "?_format=json";
+    axios.get(lookupUrl, {headers: req.headers}).then((x)=>{
+      // get path
+      if (x && x['field_iip_path'] && x['field_iip_path'].length && x['field_iip_path']['value']) {
+        let filepath = x['field_iip_path']['value'];
+        // issue token including this slidepath as activeSlide
+        let token = req.tokenInfo
+        token.activeSlide = filepath;
+        res.data = issueToken(token, auth.PRIKEY)
+        next();
+      }else{
+        // do not issue token
+        let err = {};
+        err.message = "unauthorized token request";
+        err.statusCode = 401;
+        next(err);
+      }
+    }).catch((e)=>{
+      console.error(e);
+      next(e);
+    });
+  } else {
+    let err = {};
+    err.message = "malformed token request";
+    err.statusCode = 400;
+    next(err);
+  }
 };
 
-slideTokenCheck = function(req, res, next) {
-  if (!BYPASS_IIP_CHECK) {
-    if (req.iipFileRequested && req.iipFileRequested == req.token.activeSlide) {
-      next();
+slideTokenCheck = function(req, res, next){
+  if (!BYPASS_IIP_CHECK){
+    if (req.iipFileRequested && req.iipFileRequested == req.token.activeSlide){
+      next()
     } else {
       // do not return
       let err = {};
@@ -60,11 +60,11 @@ slideTokenCheck = function(req, res, next) {
       next(err);
     }
   } else {
-    next();
+    next()
   }
 };
 
-let pih = {};
+let pih = {}
 pih.slideTokenGen = slideTokenGen;
 pih.slideTokenCheck = slideTokenCheck;
 
