cc23944af1c2b90a8f60593680d5b5fa13ebc7df
Ryan Birmingham <birm@rbirm.us>
2022-08-16 14:11:56 -0400

    file handler, add uuid pkg

---
+++ handlers/fileHandlers.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises;
+const uuid = require("uuid");
+
+// write a file containing the request body
+function writeFile(path, prefix) {
+  return function(req, res, next) {
+    let fn = prefix + "_" + uuid.v4();
+    var data = JSON.parse(req.body);
+    fs.writeFile(path + fn, data).then(()=>next()).catch((e) => next(e));
+  };
+};
+
+fileHandlers = {};
+fileHandlers.writeFile = writeFile
