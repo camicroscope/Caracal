d5e91d06d94795c602c760bde3a669a86c8f86fb
Ryan Birmingham <birm@rbirm.us>
2023-01-06 15:44:38 -0500

    support use of a file prefix for batches

--- handlers/fileHandlers.js
+++ handlers/fileHandlers.js
@@ -1,9 +1,13 @@
 const fs = require('fs').promises;
 const uuid = require("uuid");
+var file_prefix = process.env.file_prefix;
 
 // write a file containing the request body
 function writeFile(path, prefix) {
   return function(req, res, next) {
+    if (file_prefix){
+      prefix = file_prefix + "_" + prefix;
+    }
     let fn = prefix + "_" + uuid.v4() + ".json";
     const json = JSON.parse(req.body);
     json.create_date = new Date();
