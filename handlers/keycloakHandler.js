846afe920a2cf124cb9d5c9ee7fe7e00fa4ec4fe
Ryan Birmingham <birm@rbirm.us>
2023-01-25 19:08:57 -0500

    add user function for keycloak

--- handlers/keycloakHandler.js
+++ handlers/keycloakHandler.js
@@ -3,23 +3,76 @@ const fetch = require('cross-fetch');
 const KEYCLOAK_ADMIN = process.env.KEYCLOAK_ADMIN || "admin";
 const KEYCLOAK_ADMIN_PASSWORD = process.env.KEYCLOAK_ADMIN_PASSWORD || "password";
 const KEYCLOAK_HOST = process.env.KEYCLOAK_HOST || "http://ca-key:8080";
+const KEYCLOAK_APP_REALM = process.env.KEYCLOAK_APP_REALM || "camic";
 
-async function createUser(){
-  let userInfo = {
+async function createUser(firstName, lastName, email, username, password) {
+  let adminInfo = {
     'username': KEYCLOAK_ADMIN,
     'password': KEYCLOAK_ADMIN_PASSWORD,
     'grant_type': "password",
-    'client_id': "admin-cli"
-  }
-  let adminKeys = await fetch(KEYCLOAK_HOST + '/realms/master/protocol/openid-connect/token',{
+    'client_id': "admin-cli",
+  };
+  let adminKeys = await fetch(KEYCLOAK_HOST + '/realms/master/protocol/openid-connect/token', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
+      'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: new URLSearchParams(userInfo)
-  }).then(x=>x.json())
-  console.log(adminKeys)
-  // get admin token
-  ////-- https://www.appsdeveloperblog.com/keycloak-rest-api-create-a-new-user/
+    body: new URLSearchParams(adminInfo),
+  }).then((x)=>x.json());
   // add user
+  let userDetail = {
+    "firstName": firstName,
+    "lastName": lastName,
+    "email": email,
+    "username": username,
+    "enabled": "true",
+  };
+  // create the user in keycloak.
+  // this call doesn't seem to return anything except a status code?
+  fetch(KEYCLOAK_HOST + '/admin/realms/' + KEYCLOAK_APP_REALM + '/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + adminKeys['access_token'],
+    },
+    body: JSON.stringify(userDetail),
+  }).then(async (response)=>{
+    // Important! if userCreate fails, it's probably because the user already exists.
+    // DO NOT set/reset password for users which already exist this way.
+    if (response.status >= 400) {
+      throw new Error("User create gave status code " + response.status);
+    } else {
+      console.info("Created User " + username);
+      // get all user ids, then find the one we just made
+      let userInfo = await fetch(KEYCLOAK_HOST + '/admin/realms/' + KEYCLOAK_APP_REALM + '/users', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + adminKeys['access_token'],
+        },
+      }).then((x)=>x.json());
+      let thisUserInfo = userInfo.filter((y)=>y['username'] == username);
+      if (thisUserInfo && thisUserInfo.length && thisUserInfo[0]['id']) {
+        let passwordResetBody = {
+          "type": "password",
+          "temporary": false,
+          "value": password,
+        };
+        let passwordReset = await fetch(KEYCLOAK_HOST + '/admin/realms/' +
+            KEYCLOAK_APP_REALM + '/users/' + thisUserInfo[0]['id'] + '/reset-password', {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + adminKeys['access_token'],
+          },
+          body: JSON.stringify(passwordResetBody),
+        });
+        console.info("Set password for " + username);
+      } else {
+        throw new Error("User was not successfully created/found for username " + username);
+      }
+    }
+  }).catch((e)=>{
+    console.error(e);
+  });
 }
