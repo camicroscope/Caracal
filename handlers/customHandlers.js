3e4e68d170e310c31fde0d47c70c76e39591330e
Ryan Birmingham <birm@rbirm.us>
2023-03-24 10:10:30 -0400

    email change

--- handlers/customHandlers.js
+++ handlers/customHandlers.js
@@ -1,11 +1,30 @@
 const sendmail = require('sendmail')({silent: true});
 const kc = require('./keycloakHandlers.js');
 const auth = require('./authHandlers.js');
+const sgMail = require('@sendgrid/mail');
 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 let adminAddress = process.env.ADMIN_EMAIL || "rbirmin@emory.edu";
 let resetURL = process.env.RESET_URL || "https://wolf.cci.emory.edu/camic_htt/apps/registration/resetPassword.html";
 
 // handlers for special routes
+
+function sendMail(to, subject, message) {
+  const msg = {
+    to: to, // Change to your recipient
+    from: adminAddress, // Must be a "verified sender"
+    subject: subject,
+    html: message,
+  };
+  return sgMail
+      .send(msg)
+      .then(() => {
+        console.log('Email sent');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+}
 
 function userRegistrationFlow() {
   return function(req, res, next) {
@@ -18,35 +37,10 @@ function userRegistrationFlow() {
     // console.log(username, " - ", password)
     // kc signup
     kc.addKcUser(firstName, lastName, email).then(()=>{
-      // email to user at email with password
-      let userEmail = `<p>Your account has been created with limited access, and will be given more complete access after being reviewed by the administrative team.</p>
-                            <hr>
-                            <p><b>Username:</b> ${email}</p>
-
-                            <p>Thank you for participating.</p>`;
-      sendmail({
-        from: adminAddress,
-        to: email,
-        subject: "Your caMicroscope login",
-        html: userEmail,
-      },
-      function(err, reply) {
-        console.log(err && err.stack);
-        console.dir(reply);
-      });
       // email to admin that new low-power user created.
-      let adminEmail = `<p>A new account for ${firstName} ${lastName} with email ${email} has been created with limited access.
+      let adminMsg = `<p>A new account for ${firstName} ${lastName} with email ${email} has been created with limited access.
                            Check the user queue to assign this and other users to collections.</p>`;
-      sendmail({
-        from: adminAddress,
-        to: adminAddress,
-        subject: "New caMicroscope user",
-        html: adminEmail,
-      },
-      function(err, reply) {
-        console.log(err && err.stack);
-        console.dir(reply);
-      });
+      sendMail(adminAddress, "caMicroscope : New User Sign up", adminMsg);
       res.json({'username': email});
       next();
     }).catch((e)=>next(e));
@@ -68,22 +62,12 @@ function requestResetPassword() {
     }
     let token = auth.makeJwt(user, auth.PRIKEY, auth.EXPIRY);
     // send email to address with link with token
-    let resetEmail = `<p>Go the following page to resetr your caMicroscope password. Ignore this email if this request is in error.<br/>
+    let resetMsg = `<p>Welcome to caMicroscope! Please go the following page to resetr your caMicroscope password. Ignore this email if this request is in error.<br/>
                         <a href="${resetURL + "?token=" + token}>Reset your password</a>"
                          </p>`;
     // TODO remove this log for prod
-    console.log(resetEmail)
-    sendmail({
-      from: adminAddress,
-      to: email,
-      subject: "caMicroscope Password Reset",
-      html: resetEmail,
-    },
-    function(err, reply) {
-      console.log(err && err.stack);
-      console.dir(reply);
-      next(err);
-    });
+    console.log(resetEmail);
+    sendMail(email, "caMicroscope : Welcome", resetMsg);
     res.json({'username': email});
     next();
   };
